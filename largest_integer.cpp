48204a849d60dbc2c516f1ecc92e3310c72a328d
BPK2002 <141432799+BPK2002@users.noreply.github.com>
2023-11-07 23:02:28 +0530

    Update and rename ex1.cpp to largest_integer.cpp

---
+++ largest_integer.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+long largest_integer(int k){
+  if(k < 2 || k > 9){
+    cout<<"Invalid number to divide with."<<endl;
+    return 0;
+  }
+  long N = 0;
+  for(int i = 0; i < k ; i++){
+    N *= 10;
+    N += k - 1;
+  }
+  return N;
+}
+
+int main(){
+  int k;
+  cin >> k;
+  long N = largest_integer(k);
+  cout << "The largest integer is "<<N<<endl;
+  retrun 0;
+}
